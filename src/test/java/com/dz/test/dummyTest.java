1d121262f267b51a8a7f454c9e0f2eb9529b0390
daweizhuang <dawei.zhuang@antra.net>
2016-09-15 16:49:33 -0400

    Dashboard Document.

--- src/test/java/com/dz/test/dummyTest.java
+++ src/test/java/com/dz/test/dummyTest.java
@@ -2,16 +2,18 @@ package com.dz.test;
 
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by daweizhuang on 8/26/16.
  */
-public class dummy {
+public class dummyTest {
     @Test
     public void testBcrypt(){
        BCryptPasswordEncoder bCEncoder =  new BCryptPasswordEncoder();
         System.out.println(bCEncoder.encode("1"));
 
         System.out.println(bCEncoder.matches("1", "$2a$10$buNS49tuUVqCzKcAyqxAT.73H/9I44C43B4iJcq4IK0dB3uqodb0G"));
+        assertTrue(false);
     }
 }
